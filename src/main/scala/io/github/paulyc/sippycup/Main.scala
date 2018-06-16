0ed6da52352b78f5620dcd0ea57138ef736a5573
Paul Ciarlo <paul.ciarlo@gmail.com>
2018-06-15 22:32:25 -0400

    Initial sbt project and jain-sip libs and dependencies and main class

---
+++ src/main/scala/io/github/paulyc/sippycup/Main.scala
@@ -0,0 +1,9 @@
+package io.github.paulyc.sippycup
+
+import akka.actor.{ Actor, ActorLogging, ActorRef, ActorSystem, Props }
+
+object Main {
+  def main(args: Array[String]) {
+    println("Hello SIPpyCup!")
+  }
+}
